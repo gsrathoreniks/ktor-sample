7ab7973791aa4c81686db3364f6da68d0eca9b84
gsrathoreniks <niks.gajsa@gmail.com>
2022-04-22 10:54:22 +0530

    comments added for configuring Serialization

--- src/main/kotlin/com/example/models/Customer.kt
+++ src/main/kotlin/com/example/models/Customer.kt
@@ -5,6 +5,10 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Customer(val id: String, val firstName: String, val lastName: String, val email: String)
 
+
+/* Hardcoded list of customers for understanding the concepts of HTTP-REQUESTS (GET, POST, DELETE, PUT)
+* Without worrying about the database connection and complexities.
+*/
 val customerStorage = mutableListOf(
     Customer("1","Amit","Birthalia","amit@gmail.com"),
     Customer("2","Avinash","Singh","avi@gmail.com"),
--- src/main/kotlin/com/example/plugins/Serialization.kt
+++ src/main/kotlin/com/example/plugins/Serialization.kt
@@ -4,8 +4,18 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 
+/*
+* Extension function for SERIALIZATION
+* */
 fun Application.configureSerialization() {
+
+    /*
+    *  Installing [ContentNegotiation] for Negotiating media-types between
+    *  Client and Server, it uses the ACCEPT & CONTENT-TYPE headers for this
+    * */
     install(ContentNegotiation) {
+        /*Registers the application/json (or another specified contentType)
+         content type to the ContentNegotiation plugin using kotlinx.serialization*/
         json()
     }
 }
